0f3ebc7faf913754ae1de1a292d2aede338b0861
bossbossk20 <thanawatck@gmail.com>
2017-09-11 12:22:26 +0700

    notification to status bar

--- app/src/main/java/asia/sendit/testnoti/MyFirebaseMessagingService.java
+++ app/src/main/java/asia/sendit/testnoti/MyFirebaseMessagingService.java
@@ -1,6 +1,14 @@
 package asia.sendit.testnoti;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Handler;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -16,12 +24,38 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
         final String text = remoteMessage.getData().get("txt");
+        final String box = remoteMessage.getData().get("box");
         Log.d("noti", text+"");
+        sendNotification(text, box);
         handler.post(new Runnable() {
             @Override
             public void run() {
                 Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT).show();
             }
         });
+    }
+    private void sendNotification(String txt, String box) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("message", box);
+
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        stackBuilder.addParentStack(MainActivity.class);
+        stackBuilder.addNextIntent(intent);
+        PendingIntent pendingIntent =
+                stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle(txt)
+                .setContentText(box)
+                .setAutoCancel(true)
+                .setSound(defaultSoundUri)
+                .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
     }
 }
